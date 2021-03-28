174ec1d03ef2b805222cfd18e407f7f6fe928f92
Maxim <maxim.makarov14@gmail.com>
2021-03-28 15:13:23 +0300

    Created position constant

---
+++ src/constants/positions.js
@@ -0,0 +1,61 @@
+const POSITION = {
+  't-l': { title: 'top-left', calc: () => ({ x: 0, y: 0 }) },
+  't-c': {
+    title: 'top-center',
+    calc: (item, canvas) => ({
+      x: canvas.width / 2 - item.width / 2,
+      y: 0
+    })
+  },
+  't-r': {
+    title: 'top-right',
+    calc: (item, canvas) => ({
+      x: canvas.width - item.width,
+      y: 0
+    })
+  },
+
+  'c-l': {
+    title: 'center-left',
+    calc: (item, canvas) => ({
+      x: 0,
+      y: canvas.height / 2 - item.height / 2
+    })
+  },
+
+  'c-c': {
+    title: 'center-center',
+    calc: (item, canvas) => ({
+      x: canvas.width / 2 - item.width / 2,
+      y: canvas.height / 2 - item.height / 2
+    })
+  },
+  'c-r': {
+    title: 'center-right',
+    calc: (item, canvas) => ({
+      x: canvas.width - item.width,
+      y: canvas.height / 2 - item.height / 2
+    })
+  },
+
+  'b-l': {
+    title: 'bottom-left',
+    calc: (item, canvas) => ({ x: 0, y: canvas.height - item.height })
+  },
+  'b-c': {
+    title: 'bottom-center',
+    calc: (item, canvas) => ({
+      x: canvas.width / 2 - item.width / 2,
+      y: canvas.height - item.height
+    })
+  },
+  'b-r': {
+    title: 'bottom-right',
+    calc: (item, canvas) => ({
+      x: canvas.width - item.width,
+      y: canvas.height - item.height
+    })
+  }
+}
+
+export default POSITION
