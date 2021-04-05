f90e0b6331427c67f9e5ec44b386d05adde7e2ff
Maxim <maxim.makarov14@gmail.com>
2021-04-06 00:09:07 +0300

    Changed TypographyForm layout

--- src/components/forms/TypographyForm/TypographyForm.js
+++ src/components/forms/TypographyForm/TypographyForm.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import { useState } from 'react'
-import { Input, InputNumber, Select, Space } from 'antd'
+import { Collapse, Input, InputNumber, Select, Space } from 'antd'
 import { Title, Row, Col, Box, Text } from 'antd-styled'
 import { FontColorsOutlined, FontSizeOutlined } from '@ant-design/icons'
 import _ from 'lodash'
@@ -50,117 +50,128 @@ const TypographyForm = (props) => {
   return (
     <>
       <GoogleFontLoader fonts={fonts} />
-      <Row>
-        <Col span={24}>
-          <Title level={3}>Typography</Title>
-        </Col>
-        <Col span={24}>
-          <Row gutter={[8, 8]}>
+      <Collapse
+        defaultActiveKey={['1']}
+        expandIconPosition="right"
+        style={{ margin: '-16px' }}
+        ghost>
+        <Collapse.Panel
+          header={
+            <Title level={3} style={{ marginBottom: 0 }}>
+              Typography
+            </Title>
+          }
+          key="1">
+          <Row>
             <Col span={24}>
-              <Input
-                value={textConfig.titleText}
-                placeholder="Title"
-                onChange={(e) => {
-                  setTextConfig({
-                    ...textConfig,
-                    titleText: e.target.value || undefined
-                  })
-                }}
-              />
-            </Col>
-            <Col span={24}>
-              <TextArea
-                value={textConfig.subTitleText}
-                placeholder="Subtitle"
-                onChange={(e) => {
-                  setTextConfig({
-                    ...textConfig,
-                    subTitleText: e.target.value || undefined
-                  })
-                }}
-              />
-            </Col>
-            <Col span={24}>
-              <FontFamilySelect
-                value={textConfig.textFontFamily}
-                onFontsLoaded={onFontsLoaded}
-                onFontSelect={(fontFamily, fonts) => {
-                  setTextConfig({
-                    ...textConfig,
-                    textFontFamily: fontFamily,
-                    textFontWeight: FONT_WEIGHTS['regular'].value
-                  })
-                  setWeights(_.find(fonts, ['font', fontFamily])?.weights)
-                }}
-              />
-            </Col>
-            <Col span={24}>
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center">
-                <Text>Text Font Weight</Text>
-                <Select
-                  value={textConfig.textFontWeight}
-                  options={weights?.map((weight) => ({
-                    label: (
-                      <Text
-                        style={{
-                          fontWeight: FONT_WEIGHTS[weight].value,
-                          fontStyle: FONT_WEIGHTS[weight]?.style
-                        }}>
-                        {FONT_WEIGHTS[weight].label}
-                      </Text>
-                    ),
-                    value: FONT_WEIGHTS[weight].value
-                  }))}
-                  loading={loading}
-                  onSelect={(weight) => {
-                    setTextConfig({
-                      ...textConfig,
-                      textFontWeight: weight
-                    })
-                  }}
-                  style={{ minWidth: '150px' }}
-                />
-              </Box>
-            </Col>
-            <Col span={24}>
-              <FontSizeInput
-                label="Title Font Size"
-                value={textConfig.titleFontSize}
-                onChange={(size) =>
-                  setTextConfig({ ...textConfig, titleFontSize: size })
-                }
-              />
-            </Col>
-            <Col span={24}>
-              <FontSizeInput
-                label="Sub Title Font Size"
-                value={textConfig.subTitleFontSize}
-                onChange={(size) =>
-                  setTextConfig({ ...textConfig, subTitleFontSize: size })
-                }
-              />
-            </Col>
-            <Col span={24}>
-              <PositionSelect
-                onSelect={(value) =>
-                  setTextConfig({ ...textConfig, placement: value })
-                }
-                value={textConfig.placement}
-              />
-            </Col>
-            <Col span={24}>
-              <ColorPicker
-                color={textConfig.color}
-                onChange={onColorChange}
-                icon={<FontColorsOutlined />}
-              />
+              <Row gutter={[8, 8]}>
+                <Col span={24}>
+                  <Input
+                    value={textConfig.titleText}
+                    placeholder="Title"
+                    onChange={(e) => {
+                      setTextConfig({
+                        ...textConfig,
+                        titleText: e.target.value || undefined
+                      })
+                    }}
+                  />
+                </Col>
+                <Col span={24}>
+                  <TextArea
+                    value={textConfig.subTitleText}
+                    placeholder="Subtitle"
+                    onChange={(e) => {
+                      setTextConfig({
+                        ...textConfig,
+                        subTitleText: e.target.value || undefined
+                      })
+                    }}
+                  />
+                </Col>
+                <Col span={24}>
+                  <FontFamilySelect
+                    value={textConfig.textFontFamily}
+                    onFontsLoaded={onFontsLoaded}
+                    onFontSelect={(fontFamily, fonts) => {
+                      setTextConfig({
+                        ...textConfig,
+                        textFontFamily: fontFamily,
+                        textFontWeight: FONT_WEIGHTS['regular'].value
+                      })
+                      setWeights(_.find(fonts, ['font', fontFamily])?.weights)
+                    }}
+                  />
+                </Col>
+                <Col span={24}>
+                  <Box
+                    display="flex"
+                    justifyContent="space-between"
+                    alignItems="center">
+                    <Text>Text Font Weight</Text>
+                    <Select
+                      value={textConfig.textFontWeight}
+                      options={weights?.map((weight) => ({
+                        label: (
+                          <Text
+                            style={{
+                              fontWeight: FONT_WEIGHTS[weight].value,
+                              fontStyle: FONT_WEIGHTS[weight]?.style
+                            }}>
+                            {FONT_WEIGHTS[weight].label}
+                          </Text>
+                        ),
+                        value: FONT_WEIGHTS[weight].value
+                      }))}
+                      loading={loading}
+                      onSelect={(weight) => {
+                        setTextConfig({
+                          ...textConfig,
+                          textFontWeight: weight
+                        })
+                      }}
+                      style={{ minWidth: '150px' }}
+                    />
+                  </Box>
+                </Col>
+                <Col span={24}>
+                  <FontSizeInput
+                    label="Title Font Size"
+                    value={textConfig.titleFontSize}
+                    onChange={(size) =>
+                      setTextConfig({ ...textConfig, titleFontSize: size })
+                    }
+                  />
+                </Col>
+                <Col span={24}>
+                  <FontSizeInput
+                    label="Sub Title Font Size"
+                    value={textConfig.subTitleFontSize}
+                    onChange={(size) =>
+                      setTextConfig({ ...textConfig, subTitleFontSize: size })
+                    }
+                  />
+                </Col>
+                <Col span={24}>
+                  <PositionSelect
+                    onSelect={(value) =>
+                      setTextConfig({ ...textConfig, placement: value })
+                    }
+                    value={textConfig.placement}
+                  />
+                </Col>
+                <Col span={24}>
+                  <ColorPicker
+                    color={textConfig.color}
+                    onChange={onColorChange}
+                    icon={<FontColorsOutlined />}
+                  />
+                </Col>
+              </Row>
             </Col>
           </Row>
-        </Col>
-      </Row>
+        </Collapse.Panel>
+      </Collapse>
     </>
   )
 }
